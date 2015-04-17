d0aa2db22d1f96ffda2ec69f41dfc7fe733161ac
Erica <chae_osu@yahoo.com>
2015-04-17 10:31:13 -0700

    page functions, but price needs work.

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,55 @@
-var price = 0;
+$(document).ready(function() {
 
-$(document).ready(function(){
-  $("form#orderP").submit(function(event) {
+  $("form#user-input").submit(function(event) {
     event.preventDefault();
 
-  var pepperoni = { price : 2 };
-  var cheese = { price : 1 };
-
-  var cost = { amount: [pepperoni, cheese] };
-
-  cost.amount.forEach(function(total) {
-    $("#result").text(price);
-  });
-
-  var inputtedPepperoni = $(".btnP").val();
-  newPepperoni = {pepporoni: inputtedPeppori};
+    var inputtedPizza = $("input#new-pizza").val();
+    var newPizza = { order : inputtedPizza };
 
 
+    $("ul#orders").append("<li><span class='orderPizza'>" + newPizza.order + "</span></li>");
+
+    $("input#new-pizza").val("");
+
+    $(".orderPizza").last().click(function() {
+      $("#show-order").show();
+      $("#show-order h2").text(newPizza.order);
+      $(".new-pizza").text(newPizza.order);
+      
+    });
   });
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var price = 0;
+//
+// $(document).ready(function(){
+//   $("form#orderP").submit(function(event) {
+//     event.preventDefault();
+//
+//   var pepperoni = { price : 2 };
+//   var cheese = { price : 1 };
+//
+//   var cost = { amount: [pepperoni, cheese] };
+//
+//   cost.amount.forEach(function(total) {
+//     $("#result").text(price);
+//   });
+//
+//   var inputtedPepperoni = $(".btnP").val();
+//   newPepperoni = {pepporoni: inputtedPepperoni};
+//
+//
+//   });
+// });
